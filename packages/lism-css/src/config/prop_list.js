b3ed30180f988928c660a026ac20bccaac27b72d
ddryo <dd.bocsfcryo@gmail.com>
2025-06-05 01:34:00 +0900

    SPACEクラスは80まで作成

--- packages/lism-css/src/config/prop_list.js
+++ packages/lism-css/src/config/prop_list.js
@@ -1,5 +1,5 @@
 import TOKENS from './tokens';
-const SPACE_PRESETS = ['0', '5', '10', '20', '30', '40', '50'];
+const SPACE_PRESETS = ['0', '5', '10', '20', '30', '40', '50', '60', '70', '80'];
 const auto = { auto: 'a' };
 /* 
 memo: 
