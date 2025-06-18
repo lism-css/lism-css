d5f775de313c6123d00e94900a014813884b9697
ddryo <31400297+ddryo@users.noreply.github.com>
2025-06-18 15:29:15 +0900

    avatar,badge,button調整

--- apps/docs/src/components/ex/Badge/index.jsx
+++ apps/docs/src/components/ex/Badge/index.jsx
@@ -2,8 +2,13 @@ import { Lism } from 'lism-css/react';
 import './style.css';
 
 export default function Badge({ children, ...props }) {
+	// c--badge では c, bgc は 変数で受け取る
+	const _propConfig = {
+		c: { isVar: 1 },
+		bgc: { isVar: 1 },
+	};
 	return (
-		<Lism lismClass='c--badge' tag='span' px='10' py='5' lh='1' bdrs='10' {...props}>
+		<Lism lismClass='c--badge' tag='span' fz='s' lh='1' bdrs='10' _propConfig={_propConfig} {...props}>
 			{children}
 		</Lism>
 	);
--- apps/docs/src/components/index.js
+++ apps/docs/src/components/index.js
@@ -4,5 +4,6 @@ export { default as Callout } from '~/components/ex/Callout/index.jsx';
 export { default as Note } from '~/components/ex/Note/index.jsx';
 export { default as Button } from '~/components/ex/Button/index.jsx';
 export { default as ImportPackage } from './code-template/ImportPackage.astro';
-export { default as ImportSource } from './code-template/ImportSource.astro';
 export { default as EmbedCode } from './code-template/EmbedCode.astro';
+export { default as ImportSource } from './code-template/ImportSource.astro';
+export { default as ImportExComponent } from './code-template/ImportExComponent.astro';
