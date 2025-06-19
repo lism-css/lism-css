bf32e1b17dc8feae988a07e1d526534e3af85f2a
ddryo <31400297+ddryo@users.noreply.github.com>
2025-06-19 23:11:20 +0900

    List調整

--- apps/docs/src/components/ex/List/index.jsx
+++ apps/docs/src/components/ex/List/index.jsx
@@ -1,9 +1,11 @@
 import { Lism } from 'lism-css/react';
 import './style.css';
 
-export default function List({ tag = 'ul', children, ...props }) {
+export default function List({ tag = 'ul', iconC, iconImg, children, style = {}, ...props }) {
+	if (iconC) style['--iconC'] = iconC;
+	if (iconImg) style['--iconImg'] = iconImg;
 	return (
-		<Lism tag={tag} lismClass='c--list' {...props}>
+		<Lism tag={tag} lismClass='c--list' style={style} {...props}>
 			{children}
 		</Lism>
 	);
