a2f9b107724b86d12c914cee6f840c2a3f59bbc8
ddryo <31400297+ddryo@users.noreply.github.com>
2025-07-16 15:09:47 +0900

    Iconにsvg文字列を渡せるようにした

--- packages/lism-css/src/components/Icon/Icon.jsx
+++ packages/lism-css/src/components/Icon/Icon.jsx
@@ -3,10 +3,13 @@ import getProps from './getProps';
 import SVG from './SVG';
 
 export default function Icon({ children, ...props }) {
-	let { Component, lismProps, exProps } = getProps(props);
+	let { Component, lismProps, exProps = {}, content } = getProps(props);
 
 	if (Component === '_SVG_') {
 		Component = SVG;
+		if (content) {
+			exProps.__html = content;
+		}
 	}
 
 	return (
--- packages/lism-css/src/components/Icon/SVG.jsx
+++ packages/lism-css/src/components/Icon/SVG.jsx
@@ -1,25 +1,25 @@
-export default function SVG({
-	size = '1em',
-	fill = 'currentColor',
-	viewBox = '0 0 24 24',
-	path,
-	children,
-	...props
-}) {
+export default function SVG({ size = '1em', fill = 'currentColor', viewBox = '0 0 24 24', path, children, __html, ...props }) {
 	// const iconData = data[name] || {};
 	// const { viewBox = '0 0 256 256', path = '' } = iconData;
 
+	if (__html) {
+		return (
+			<svg
+				dangerouslySetInnerHTML={{ __html }}
+				{...props}
+				xmlns='http://www.w3.org/2000/svg'
+				viewBox={viewBox}
+				width={size}
+				height={size}
+				fill={fill}
+				focusable='false'
+			></svg>
+		);
+	}
+
 	// preset or childrenそのまま
 	return (
-		<svg
-			xmlns='http://www.w3.org/2000/svg'
-			viewBox={viewBox}
-			width={size}
-			height={size}
-			fill={fill}
-			focusable='false'
-			{...props}
-		>
+		<svg xmlns='http://www.w3.org/2000/svg' viewBox={viewBox} width={size} height={size} fill={fill} focusable='false' {...props}>
 			{path && <path d={path}></path>}
 			{children}
 		</svg>
