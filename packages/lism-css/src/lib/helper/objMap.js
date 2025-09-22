98e17530bd562f6b9da43941f449a4e1f28e1f62
ddryo <31400297+ddryo@users.noreply.github.com>
2025-09-22 14:57:39 +0900

    delete comment

--- packages/lism-css/src/lib/helper/objMap.js
+++ packages/lism-css/src/lib/helper/objMap.js
@@ -9,21 +9,3 @@ export default function objMap(obj, callback) {
 	});
 	return obj;
 }
-
-// 1. Object.entries() で {key: value, ... } → [[key, value], ...] に配列変換
-// 2. map()でその配列のvalue側を処理
-// 3. 再度、Object.fromEntries() で 配列から新しく object を生成
-// export default function objMap(obj, callback) {
-// 	return Object.fromEntries(
-// 		Object.entries(obj).map(([key, value]) => [key, callback(value)])
-// 	);
-// }
-
-// 1. Object.entries() で {key: value, ... } → [[key, value], ...] に配列変換
-// 2. その配列をもとに、reduce() で処理を加えつつ objectを生成
-// export default function objMap(obj, callback) {
-// 	return Object.entries(obj).reduce((newObj, [key, r]) => {
-// 		newObj[key] = callback(r);
-// 		return newObj;
-// 	}, {});
-// }
