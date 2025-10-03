46809ae99e03002da870e27023dfd9b0b5a161f0
ddryo <31400297+ddryo@users.noreply.github.com>
2025-10-03 22:30:39 +0900

    bd系調整 #4

--- packages/lism-css/src/components/Divider/getProps.js
+++ packages/lism-css/src/components/Divider/getProps.js
@@ -2,8 +2,7 @@ import atts from '../../lib/helper/atts';
 
 export function getDividerProps({ lismClass, ...props }) {
 	const defaultProps = {
-		lismClass: atts(lismClass, `l--divider`),
-		bd: 'bs',
+		lismClass: atts(lismClass, 'l--divider'),
 		'aria-hidden': 'true',
 	};
 	return { ...defaultProps, ...props };
