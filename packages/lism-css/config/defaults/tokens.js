e0e1147d6312ea00eedaed5316cf5b204686716a
ddryo <dd.bocsfcryo@gmail.com>
2025-10-12 14:22:32 +0900

    #8 size系調整

--- packages/lism-css/config/defaults/tokens.js
+++ packages/lism-css/config/defaults/tokens.js
@@ -7,7 +7,7 @@ export default {
 	o: ['ghost', 'pale', 'soft', 'mild', 'solid'],
 	bdrs: ['5', '10', '20', '30', '40', '50', '99'],
 	bxsh: ['5', '10', '20', '30', '40', '50'],
-	size: ['min', 'xs', 's', 'm', 'l', 'xl'], // --size-xxx トークン
+	size: ['min', 'xs', 's', 'm', 'l', 'xl'],
 	space: {
 		pre: '--s',
 		values: ['0', '5', '10', '20', '30', '40', '50', '60', '70', '80'],
