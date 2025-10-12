e01bb2bab678b430e19c134be05c16dfb7d58e0d
ddryo <31400297+ddryo@users.noreply.github.com>
2025-10-13 01:44:00 +0900

    dummy に img 追加

--- packages/lism-css/src/components/Dummy/Dummy.jsx
+++ packages/lism-css/src/components/Dummy/Dummy.jsx
@@ -1,14 +1,11 @@
 import { Lism } from '../Lism';
 import getContent from './getContent';
 
-export default function Dummy({
-	tag = 'p',
-	pre = '',
-	length = 'm',
-	lang = 'en',
-	offset = 0,
-	...props
-}) {
+export default function Dummy({ tag = 'p', pre = '', length = 'm', lang = 'en', offset = 0, ...props }) {
+	if (tag === 'img') {
+		return <Lism tag='img' src='https://placehold.co/300x200?text=placeholder' width='300' height='200' alt='' {...props} />;
+	}
+
 	const content = getContent({ tag, pre, lang, length, offset });
 	return <Lism tag={tag} {...props} dangerouslySetInnerHTML={{ __html: content }} />;
 }
