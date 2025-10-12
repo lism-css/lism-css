6d428b712d93de20b73c9ec609c4d359408485a7
ddryo <31400297+ddryo@users.noreply.github.com>
2025-10-12 15:01:39 +0900

    #8 --c--

--- packages/lism-css/config/defaults/tokens.js
+++ packages/lism-css/config/defaults/tokens.js
@@ -13,7 +13,7 @@ export default {
 		values: ['0', '5', '10', '20', '30', '40', '50', '60', '70', '80'],
 	},
 	c: {
-		pre: '--c-',
+		pre: '--c--',
 		values: ['base', 'base-2', 'text', 'text-2', 'line', 'link', 'main', 'accent'],
 	},
 	palette: {
