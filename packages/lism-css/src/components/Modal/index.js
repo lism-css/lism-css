ed485a3300ad42a2a8fa8f34c9ed913e4806fb87
ddryo <dd.bocsfcryo@gmail.com>
2025-10-14 13:04:34 +0900

    Modal調整

--- packages/lism-css/src/components/Modal/index.js
+++ packages/lism-css/src/components/Modal/index.js
@@ -1,8 +1,7 @@
 import Root from './Modal';
 import Inner from './Inner';
 import Body from './Body';
-import CloseIconBtn from './CloseIconBtn';
-import Header from './Header';
-import Footer from './Footer';
+import CloseBtn from './CloseBtn';
+import OpenBtn from './OpenBtn';
 
-export default { Root, Inner, Body, CloseIconBtn, Header, Footer };
+export default { Root, Inner, Body, CloseBtn, OpenBtn };
