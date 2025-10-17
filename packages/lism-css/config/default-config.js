7f90acfa544789a85c7f9b6950488c44ad52e682
ddryo <dd.bocsfcryo@gmail.com>
2025-10-17 20:33:34 +0900

    change file name

--- packages/lism-css/config/default-config.js
+++ packages/lism-css/config/default-config.js
@@ -1,6 +1,6 @@
 import TOKENS from './defaults/tokens.js';
 import PROPS from './defaults/props.js';
-import STATES from './defaults/layout-states.js';
+import STATES from './defaults/states.js';
 
 export default {
 	tokens: TOKENS,
