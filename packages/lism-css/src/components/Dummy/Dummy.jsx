1c5cb51e2c82ae8b6b826a629d905784539abf9a
ddryo <dd.bocsfcryo@gmail.com>
2025-11-01 15:21:13 +0900

    画像パス差し替え

--- packages/lism-css/src/components/Dummy/Dummy.jsx
+++ packages/lism-css/src/components/Dummy/Dummy.jsx
@@ -3,7 +3,7 @@ import getContent from './getContent';
 
 export default function Dummy({ tag = 'p', pre = '', length = 'm', lang = 'en', offset = 0, ...props }) {
 	if (tag === 'img') {
-		return <Lism tag='img' src='https://placehold.co/300x200?text=placeholder' width='300' height='200' alt='' {...props} />;
+		return <Lism tag='img' src='https://cdn.lism-css.com/dummy-image.jpg' width='600' height='400' alt='' {...props} />;
 	}
 
 	const content = getContent({ tag, pre, lang, length, offset });
