e88cb981603d5652966efd76945f48043edc5ecf
ddryo <dd.bocsfcryo@gmail.com>
2025-11-06 16:43:12 +0900

    layout処理調整

--- packages/lism-css/src/components/Lism/Lism.jsx
+++ packages/lism-css/src/components/Lism/Lism.jsx
@@ -1,10 +1,9 @@
 import getLismProps from '../../lib/getLismProps';
-import getLayoutProps from '../getLayoutProps';
 
 /**
  * Lism Propsを処理できるだけのコンポーネント
  */
-export default function Lism({ children, as, tag, layout, exProps, ...props }) {
+export default function Lism({ children, as, tag, exProps, ...props }) {
 	// tagは文字列のみ。（asはコンポーネントも指定できる。）
 	// if (tag && typeof tag !== 'string') {
 	// 	console.error('@Lism : "tag" prop should be a string.');
@@ -13,7 +12,7 @@ export default function Lism({ children, as, tag, layout, exProps, ...props }) {
 	let JSX = as || tag || 'div';
 
 	return (
-		<JSX {...getLismProps(getLayoutProps(layout, props))} {...exProps}>
+		<JSX {...getLismProps(props)} {...exProps}>
 			{children}
 		</JSX>
 	);
