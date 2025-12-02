0cc2a50ad939f613743072aaf51cdc48009e2431
ddryo <dd.bocsfcryo@gmail.com>
2025-12-02 17:06:44 +0900

    Modal調整

--- packages/lism-css/src/components/Modal/getProps.js
+++ packages/lism-css/src/components/Modal/getProps.js
@@ -1,6 +1,6 @@
 import atts from '../../lib/helper/atts';
 
-export function getProps({ lismClass = '', duration, offset, style = {}, ...props }) {
+export function getProps({ lismClass = '', duration, style = {}, ...props }) {
 	const theProps = {
 		lismClass: atts(lismClass, 'd--modal'),
 		setPlain: true,
@@ -8,16 +8,23 @@ export function getProps({ lismClass = '', duration, offset, style = {}, ...prop
 	if (duration) {
 		style['--duration'] = duration;
 	}
-	if (offset) {
-		style['--offset'] = offset;
-	}
 
 	return { tag: 'dialog', ...theProps, style, ...props };
 }
 
+export function getInnerProps({ lismClass = '', translate, style = {}, ...props }) {
+	if (translate) {
+		style['--translate'] = translate;
+	}
+	return {
+		lismClass: atts(lismClass, 'd--modal_inner'),
+		style,
+		...props,
+	};
+}
+
 export const defaultProps = {
 	body: { lismClass: 'd--modal_body' },
-	inner: { lismClass: 'd--modal_inner', layout: 'stack' },
 	closeBtn: { tag: 'button', setPlain: true, hov: 'o', d: 'in-flex' },
 	openBtn: { tag: 'button', setPlain: true, hov: 'o', d: 'in-flex' },
 };
