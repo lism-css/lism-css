50f4b8130254bed5a4c0a60e344d6929442d2f97
ddryo <dd.bocsfcryo@gmail.com>
2025-12-15 15:23:13 +0900

    カラー調整 #23

--- apps/docs/src/components/ex/Note/index.jsx
+++ apps/docs/src/components/ex/Note/index.jsx
@@ -9,10 +9,10 @@ export default function Note({ type = 'note', keycolor, icon, title, children, f
 	}
 
 	return (
-		<Stack lismClass='c--note u-cbox' keycolor={keycolor} p='15' g='10' bd-x-s bdw='4px' bdrs='5' {...props}>
+		<Stack lismClass='c--note u-cbox set-shadow' keycolor={keycolor} p='20' g='10' bdc='keycolor' bd-x-s bdw='4px' bxsh='10' bdrs='5' {...props}>
 			{title && (
-				<Flex className='c--note_head' fw='bold' ai='center' g='10'>
-					<Center className='c--note_icon' fz='xl' c='keycolor'>
+				<Flex className='c--note_head' c='keycolor' fw='bold' ai='center' g='10'>
+					<Center className='c--note_icon' fz='xl'>
 						<Icon icon={icon} />
 					</Center>
 					<span className='c--note_title'>{title}</span>
