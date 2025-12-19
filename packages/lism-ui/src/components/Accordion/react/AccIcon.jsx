23dfd085cf7790a734660214ebef3c6394dcfea0
ddryo <dd.bocsfcryo@gmail.com>
2025-12-19 14:09:20 +0900

    Accordion移動

--- packages/lism-ui/src/components/Accordion/react/AccIcon.jsx
+++ packages/lism-ui/src/components/Accordion/react/AccIcon.jsx
@@ -1,5 +1,5 @@
 import { Lism, Icon } from 'lism-css/react';
-import { getAccIconProps } from './getProps';
+import { getAccIconProps } from '../getProps';
 
 export default function AccIcon({ icon = 'caret-down', viewBox, children = null, ...props }) {
 	return <Lism {...getAccIconProps(props)}>{children || <Icon viewBox={viewBox} icon={icon} />}</Lism>;
