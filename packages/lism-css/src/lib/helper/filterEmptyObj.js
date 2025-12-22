0cecf77a90d826030ccf71d9711f915e36045ca2
torounit <mail@torounit.com>
2025-12-22 18:10:35 +0900

    fix: filterEmptyObj にて 元のオブジェクトを変更しないように。

--- packages/lism-css/src/lib/helper/filterEmptyObj.js
+++ packages/lism-css/src/lib/helper/filterEmptyObj.js
@@ -1,14 +1,35 @@
 // isEmptyObj;
 import isEmptyObj from './isEmptyObj.ts';
 
-// 0 を残すように注意
+/**
+ * オブジェクトから空の値を持つプロパティを除外した新しいオブジェクトを返す（非破壊的）
+ *
+ * 以下の値を持つプロパティが除外されます：
+ * - 空文字列 ('')
+ * - null
+ * - undefined
+ * - 空のオブジェクト ({})
+ * - 空の配列 ([])
+ *
+ * 注意：0、false などの falsy な値は保持されます
+ *
+ * @param {Object} obj - フィルタリング対象のオブジェクト
+ * @returns {Object} 空の値を除外した新しいオブジェクト
+ *
+ * @example
+ * filterEmptyObj({ a: 'foo', b: '', c: null, d: 0 })
+ * // => { a: 'foo', d: 0 }
+ */
 export default function filterEmptyObj(obj) {
+	const result = {};
 	for (const key in obj) {
 		if (obj[key] === '' || null === obj[key] || undefined === obj[key]) {
-			delete obj[key];
-		} else if (typeof obj[key] === 'object' && isEmptyObj(obj[key])) {
-			delete obj[key];
+			continue;
 		}
+		if (typeof obj[key] === 'object' && isEmptyObj(obj[key])) {
+			continue;
+		}
+		result[key] = obj[key];
 	}
-	return obj;
+	return result;
 }
