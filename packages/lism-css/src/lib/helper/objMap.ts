263e2e3c84e7f907f4d1ea3eb6172063306b9e9a
torounit <mail@torounit.com>
2025-12-22 17:11:39 +0900

    refactor: fix comment

--- packages/lism-css/src/lib/helper/objMap.ts
+++ packages/lism-css/src/lib/helper/objMap.ts
@@ -1,8 +1,10 @@
-// Objectの各値に関数を通して返す。PHP の array_map 的な.
-
-// 1. Object.keys() で keyの配列を取得
-// 2. forEach() で その key に対する値に処理を加える。
-// 3. 処理が加わったオブジェクトを返す
+/**
+ * Objectの各値に関数を通して返す。PHP の array_map 的な.
+ *
+ * 1. Object.keys() で keyの配列を取得
+ * 2. forEach() で その key に対する値に処理を加える。
+ * 3. 処理が加わったオブジェクトを返す
+ */
 export default function objMap<T extends Record<string, any>, U>(
 	obj: T,
 	callback: (value: T[keyof T]) => U
