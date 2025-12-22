c5c6ce78f1ae827ad5c711cbfb460a831ffc3b84
torounit <mail@torounit.com>
2025-12-23 00:53:49 +0900

    feature: atts to typescript

--- packages/lism-css/src/lib/helper/atts.ts
+++ packages/lism-css/src/lib/helper/atts.ts
@@ -1,16 +1,18 @@
+type Primitive = string | number | boolean | null | undefined;
+type AttsValue = Primitive | AttsValue[] | Set<string | number> | Record<string, Primitive>;
+
 /**
  * 配列と文字列だけを受け取るようにしたclassnamesやclsxの代替関数。
  *   従来の使い方でObjectを渡したいようなケースが出てきた時は、以下のように書き換えて使える。
- *   joinAtts({'foo': isFoo, 'bar': isBar });
+ *   atts({'foo': isFoo, 'bar': isBar });
  *     ↓
- *   joinAtts(isFoo && 'foo', isBar && 'bar');
+ *   atts(isFoo && 'foo', isBar && 'bar');
  *
  *   arguments&whileループとの差はなかった(結果が誤差の範囲 or その時によって反転する)
  *   flat()やfilter()を使うとシンプルになるが、処理速度が数倍になる。
  */
-// spacedAttr, attrConcat
-export default function atts(...args) {
-	let classes = [];
+export default function atts(...args: AttsValue[]): string {
+	let classes: (string | number)[] = [];
 	for (let i = 0; i < args.length; i++) {
 		const mix = args[i];
 
@@ -22,7 +24,7 @@ export default function atts(...args) {
 			classes.push(mix);
 		} else if (Array.isArray(mix)) {
 			// 0 残す
-			classes.push(...mix.filter((v) => null != v));
+			classes.push(...mix.filter((v): v is string | number => null != v));
 		} else if (mix instanceof Set) {
 			classes.push(...mix);
 		} else if (typeof mix === 'object') {
@@ -31,7 +33,6 @@ export default function atts(...args) {
 	}
 
 	// 最後に重複削除
-	// Array.from(new Set(classes))
 	classes = [...new Set(classes)];
 	return classes.join(' ').trim();
 }
