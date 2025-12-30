3e5f094a755b6105d9e108d8dd015f5be3bb0433
ddryo <31400297+ddryo@users.noreply.github.com>
2025-12-30 19:57:31 +0900

    Avatar, Badge, Button を lism-css/ui へ移動

--- packages/lism-ui/src/components/Avatar/react/Avatar.jsx
+++ packages/lism-ui/src/components/Avatar/react/Avatar.jsx
@@ -1,6 +1,7 @@
 import { Frame } from 'lism-css/react';
+// import '../_style.css';
 
-export default function Avatar({ size = '2em', src = '', alt = '', ...props }) {
+export default function Avatar({ size = '1.5em', src = '', alt = '', ...props }) {
 	return (
 		<Frame lismClass='c--avatar' ar='1/1' w={size} bdrs='99' {...props}>
 			<img src={src} alt={alt} width='100%' height='100%' decoding='async' />
