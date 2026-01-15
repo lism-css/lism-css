7436bcdaaf631740a1171607321feaf7f2faa11f
torounit <mail@torounit.com>
2026-01-15 16:49:08 +0900

    fix: lint-staged command error

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('lint-staged').Config} */
 export default {
-	'packages/lism-css/**/*.{ts,tsx}': [() => 'tsc --noEmit -p packages/lism-css/tsconfig.json'],
-	'*.{js,mjs,cjs,ts,tsx,jsx}': ['eslint --fix', 'prettier --write'],
-	'*.{scss,css}': ['stylelint --fix'],
-	'*.astro': ['eslint --fix', 'prettier --write'],
-	'*.{md,mdx,json,yaml,yml}': ['prettier --write'],
+	'packages/lism-css/**/*.{ts,tsx}': [() => 'pnpm exec tsc --noEmit -p packages/lism-css/tsconfig.json'],
+	'*.{js,mjs,cjs,ts,tsx,jsx}': ['pnpm exec eslint --fix', 'pnpm exec prettier --write'],
+	'*.{scss,css}': ['pnpm exec stylelint --fix'],
+	'*.astro': ['pnpm exec eslint --fix', 'pnpm exec prettier --write'],
+	'*.{md,mdx,json,yaml,yml}': ['pnpm exec prettier --write'],
 };
