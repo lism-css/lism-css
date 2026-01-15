948600d8de1134faed3d6948fed1db1795ccb905
torounit <mail@torounit.com>
2026-01-15 16:51:21 +0900

    fix: typecheck

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('lint-staged').Config} */
 export default {
-	'packages/lism-css/**/*.{ts,tsx}': [() => 'pnpm exec tsc --noEmit -p packages/lism-css/tsconfig.json'],
+	'packages/lism-css/**/*.{ts,tsx}': [() => 'pnpm --filter lism-css exec tsc --noEmit'],
 	'*.{js,mjs,cjs,ts,tsx,jsx}': ['pnpm exec eslint --fix', 'pnpm exec prettier --write'],
 	'*.{scss,css}': ['pnpm exec stylelint --fix'],
 	'*.astro': ['pnpm exec eslint --fix', 'pnpm exec prettier --write'],
