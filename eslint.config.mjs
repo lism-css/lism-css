2dce41b37d12e83f6610baf2c5f566d3e2555c4a
ddryo <31400297+ddryo@users.noreply.github.com>
2026-01-23 16:51:11 +0900

    fix: eslintのignore設定にplaygroundsを追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ import tseslint from 'typescript-eslint';
 
 export default defineConfig(
 	{
-		ignores: ['**/dist/**', '**/node_modules/**', '**/.turbo/**'],
+		ignores: ['**/dist/**', '**/node_modules/**', '**/.turbo/**', 'apps/playgrounds/**'],
 	},
 	eslintConfigPrettier,
 	eslint.configs.recommended,
