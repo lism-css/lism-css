a1fd8f4e7ec95fee26810d1dfd939bfab9f180d1
torounit <mail@torounit.com>
2026-01-31 10:46:42 +0900

    fix: import JSX form react

--- packages/lism-css/src/components/Lism/Lism.tsx
+++ packages/lism-css/src/components/Lism/Lism.tsx
@@ -1,4 +1,4 @@
-import type { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react';
+import type { ComponentPropsWithoutRef, ElementType, ReactNode, JSX } from 'react';
 import getLismProps, { type LismProps } from '../../lib/getLismProps';
 
 /**
