cd3b7f0ddd79bc20b98203986db7b2fb1b2619b2
torounit <mail@torounit.com>
2026-02-13 15:29:40 +0900

    fix: Dummy support Generics

--- packages/lism-css/src/components/Dummy/Dummy.tsx
+++ packages/lism-css/src/components/Dummy/Dummy.tsx
@@ -1,18 +1,21 @@
+import type { ElementType } from 'react';
 import { Lism, type LismComponentProps } from '../Lism';
 import getContent from './getContent';
 
-type DummyProps = Omit<LismComponentProps, 'as'> & {
+type DummyProps<T extends ElementType = 'div'> = LismComponentProps<T> & {
 	pre?: string;
 	length?: string;
 	lang?: 'ja' | 'en' | 'ar';
 	offset?: number;
 };
 
-export default function Dummy({ tag = 'p', pre = '', length = 'm', lang = 'en', offset = 0, ...props }: DummyProps) {
-	if (tag === 'img') {
-		return <Lism as='img' src='https://cdn.lism-css.com/dummy-image.jpg' width={600} height={400} alt='' {...props} />;
+export default function Dummy<T extends ElementType = 'div'>({ pre = '', length = 'm', lang = 'en', offset = 0, ...props }: DummyProps<T>) {
+	if (props.as === 'img') {
+		return (
+			<Lism as='img' src='https://cdn.lism-css.com/dummy-image.jpg' width={600} height={400} alt='' {...(props as LismComponentProps<'img'>)} />
+		);
 	}
 
-	const content = getContent({ tag, pre, lang, length, offset });
-	return <Lism tag={tag} {...props} dangerouslySetInnerHTML={{ __html: content }} />;
+	const content = getContent({ tag: props.as as string, pre, lang, length, offset });
+	return <Lism {...(props as LismComponentProps)} dangerouslySetInnerHTML={{ __html: content }} />;
 }
