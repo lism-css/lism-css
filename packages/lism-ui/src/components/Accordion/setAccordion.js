784618ec9e96fe43b6b4af8fbd36404495ec12c9
ddryo <31400297+ddryo@users.noreply.github.com>
2026-02-16 00:51:53 +0900

    Astroのclient:load時を考慮して変数セットの処理を調整 #51

--- packages/lism-ui/src/components/Accordion/setAccordion.js
+++ packages/lism-ui/src/components/Accordion/setAccordion.js
@@ -6,7 +6,7 @@ export const waitAnimation = async (panel) => {
 	const animations = panel.getAnimations();
 
 	// アニメーションがなければ 'finished' を返す
-	if (animations.length === 0) return 'finished';
+	if (animations.length === 0) return 'none';
 
 	// allSettled を使うことで、キャンセル時も例外にならずに結果を取得できる
 	const results = await Promise.allSettled(animations.map((a) => a.finished));
@@ -26,9 +26,9 @@ export const maybePauseAnimation = (panel) => {
 let ACCORDION_HIDDEN_VALUE = 'until-found';
 
 // アコーディオン要素から必要な要素を取得
-const getAccordionElements = (accordion) => {
-	const heading = accordion.querySelector('.c--accordion_heading');
-	const panel = accordion.querySelector('.c--accordion_panel');
+const getAccordionElements = (accordionItem) => {
+	const heading = accordionItem.querySelector('.c--accordion_heading');
+	const panel = accordionItem.querySelector('.c--accordion_panel');
 	return {
 		heading,
 		button: heading.querySelector('button'),
@@ -39,8 +39,8 @@ const getAccordionElements = (accordion) => {
 
 // 親に data-allow-multiple がついていなければ、展開中のアコーディオンを閉じる
 //   Memo: 自身の除外処理はしていない( 開く動作の前に呼び出す & もし自分が含まれていても連打可能なため )
-const maybeCloseOpenedItems = (accordion) => {
-	const parent = accordion.parentNode;
+const maybeCloseOpenedItems = (accordionItem) => {
+	const parent = accordionItem.parentNode;
 	if (!parent) return;
 
 	// 親の data-allow-multiple 属性の有無をチェック
@@ -50,8 +50,8 @@ const maybeCloseOpenedItems = (accordion) => {
 	parent.querySelectorAll(':scope > [data-opened]').forEach((_a) => closeAccordion(_a));
 };
 
-async function openAccordion(accordion) {
-	const { panel, content, button } = getAccordionElements(accordion);
+async function openAccordion(accordionItem) {
+	const { panel, content, button } = getAccordionElements(accordionItem);
 
 	// アニメーションがあれば一時停止
 	maybePauseAnimation(panel);
@@ -63,69 +63,71 @@ async function openAccordion(accordion) {
 	await waitFrame();
 
 	// 次フレームで高さセット（ 目標の高さ = content の高さ ）
-	panel.style.setProperty('--height--opened', `${content.offsetHeight}px`);
+	accordionItem.style.setProperty('--_panel-h', `${content.offsetHeight}px`);
 
 	// さらに1フレーム待機
 	await waitFrame();
 
 	// アニメーション開始
-	accordion.setAttribute('data-opened', '');
+	accordionItem.setAttribute('data-opened', '');
 	button.setAttribute('aria-expanded', 'true');
 
 	// アニメーションを待つ
 	const status = await waitAnimation(panel);
 
-	// アニメーションが最後まで完了した時、--height--opened削除（高さセットしたままだとリサイズできない）
-	if ('finished' === status) {
-		panel.style.removeProperty('--height--opened');
+	// アニメーションが最後まで完了した時、--_panel-h削除（高さセットしたままだとリサイズできない）
+	if ('canceled' !== status) {
+		accordionItem.style.removeProperty('--_panel-h');
 	}
 }
 
-async function closeAccordion(accordion) {
-	const { panel, button } = getAccordionElements(accordion);
+async function closeAccordion(accordionItem) {
+	const { panel, button } = getAccordionElements(accordionItem);
 
 	// アニメーションがあれば一時停止
 	maybePauseAnimation(panel);
 
-	// 現在の高さをセットする
-	panel.style.setProperty('--height--opened', `${panel.offsetHeight}px`);
+	// 現在の高さを一旦セットする
+	accordionItem.style.setProperty('--_panel-h', `${panel.offsetHeight}px`);
 
 	// 1フレーム待機
 	await waitFrame();
 
-	// 次フレームで属性を削除してアニメーション開始
-	accordion.removeAttribute('data-opened');
+	// 次フレームで属性を削除
+	accordionItem.removeAttribute('data-opened');
 	button.setAttribute('aria-expanded', 'false');
+
+	// 変数削除して 0px へ向けてアニメーション開始
+	accordionItem.style.removeProperty('--_panel-h');
 
 	// アニメーションを待つ
 	const status = await waitAnimation(panel);
 
-	// アニメーションが最後まで完了した時、hidden付与 & --height--opened削除
-	if ('finished' === status) {
+	// アニメーションが最後まで完了したら、hidden付与
+	if ('canceled' !== status) {
 		panel.setAttribute('hidden', ACCORDION_HIDDEN_VALUE);
-		panel.style.removeProperty('--height--opened');
 	}
 }
 
 // アコーディオンのトグル処理
-function toggleAccordion(accordion) {
-	if (accordion.hasAttribute('data-opened')) {
+function toggleAccordion(accordionItem) {
+	if (accordionItem.hasAttribute('data-opened')) {
 		// 自身を閉じる
-		closeAccordion(accordion);
+		closeAccordion(accordionItem);
 	} else {
 		// 親に data-allow-multiple がついていなければ、他のアコーディオンを閉じる
-		maybeCloseOpenedItems(accordion);
+		maybeCloseOpenedItems(accordionItem);
 
 		// 自身を開く
-		openAccordion(accordion);
+		openAccordion(accordionItem);
 	}
 }
 
 /**
  * 個別のアコーディオンにイベントをセット（React用にクリーンアップ関数を返す）
  */
-export const setEvent = (accordion) => {
-	const { button, panel } = getAccordionElements(accordion);
+export const setEvent = (accordionItem) => {
+	const { button, panel } = getAccordionElements(accordionItem);
 
 	// until-found のオン・オフが使い分けれるように、最初に初期値を取得
 	if (panel.hasAttribute('hidden')) {
@@ -135,13 +137,13 @@ export const setEvent = (accordion) => {
 	// clickイベント登録
 	const _clickEvent = (e) => {
 		e.preventDefault(); // hidden="until-found" の自動付け外しを無効化
-		toggleAccordion(accordion);
+		toggleAccordion(accordionItem);
 	};
 
 	// beforematchイベント登録 (ページ検索時などはこっちが発火する)
 	const _beforematchEvent = (e) => {
 		e.preventDefault(); // hidden="until-found" の自動付け外しを無効化
-		toggleAccordion(accordion);
+		toggleAccordion(accordionItem);
 	};
 
 	button.addEventListener('click', _clickEvent);
@@ -159,8 +161,8 @@ export const setEvent = (accordion) => {
  */
 const setAccordion = () => {
 	const accordionAll = document.querySelectorAll('.c--accordion_item');
-	accordionAll.forEach((accordion) => {
-		setEvent(accordion);
+	accordionAll.forEach((accordionItem) => {
+		setEvent(accordionItem);
 	});
 };
 export default setAccordion;
