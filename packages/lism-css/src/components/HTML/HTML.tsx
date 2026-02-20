98f1e619ec1c777a6a4393d70f27265c308288af
torounit <mail@torounit.com>
2026-02-20 14:13:14 +0900

    fix: as を用いるように修整

--- packages/lism-css/src/components/HTML/HTML.tsx
+++ packages/lism-css/src/components/HTML/HTML.tsx
@@ -1,19 +1,19 @@
 import { Lism, type LismComponentProps } from '../Lism';
 
 export function div(props: LismComponentProps<'div'>) {
-	return <Lism tag='div' {...props} />;
+	return <Lism as='div' {...props} />;
 }
 
 export function p(props: LismComponentProps<'p'>) {
-	return <Lism tag='p' {...props} />;
+	return <Lism as='p' {...props} />;
 }
 
 export function span(props: LismComponentProps<'span'>) {
-	return <Lism tag='span' {...props} />;
+	return <Lism as='span' {...props} />;
 }
 
 export function a(props: LismComponentProps<'a'>) {
-	return <Lism tag='a' {...props} />;
+	return <Lism as='a' {...props} />;
 }
 
 type HeadingLevel = '1' | '2' | '3' | '4' | '5' | '6';
@@ -24,25 +24,25 @@ type HeadingProps = LismComponentProps<'h1'> & {
 
 export function h({ lv = '1', ...props }: HeadingProps) {
 	const tag: HeadingTag = `h${lv}`;
-	return <Lism tag={tag} {...props} />;
+	return <Lism as={tag} {...props} />;
 }
 
 export function img(props: LismComponentProps<'img'>) {
-	return <Lism tag='img' {...props} />;
+	return <Lism as='img' {...props} />;
 }
 
 export function ul(props: LismComponentProps<'ul'>) {
-	return <Lism tag='ul' {...props} />;
+	return <Lism as='ul' {...props} />;
 }
 
 export function ol(props: LismComponentProps<'ol'>) {
-	return <Lism tag='ol' {...props} />;
+	return <Lism as='ol' {...props} />;
 }
 
 export function li(props: LismComponentProps<'li'>) {
-	return <Lism tag='li' {...props} />;
+	return <Lism as='li' {...props} />;
 }
 
 export function button(props: LismComponentProps<'button'>) {
-	return <Lism tag='button' {...props} />;
+	return <Lism as='button' {...props} />;
 }
--- packages/lism-css/src/components/atomic/Media/Media.tsx
+++ packages/lism-css/src/components/atomic/Media/Media.tsx
@@ -2,5 +2,5 @@ import { Lism } from '../../Lism';
 import getProps, { type MediaProps } from './getProps';
 
 export default function Media(props: MediaProps) {
-	return <Lism tag='img' {...getProps(props)} />;
+	return <Lism as='img' {...getProps(props)} />;
 }
