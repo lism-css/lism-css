b81775ba41dae91b02eb73eaf8e517c0846b6d31
ddryo <dd.bocsfcryo@gmail.com>
2026-02-22 15:19:12 +0900

    Accordion調整

--- packages/lism-ui/src/components/Accordion/getProps.js
+++ packages/lism-ui/src/components/Accordion/getProps.js
@@ -2,7 +2,7 @@ import atts from 'lism-css/lib/helper/atts';
 
 export function getRootProps({ lismClass, allowMultiple, ...props }) {
 	props.lismClass = atts(lismClass, 'c--accordion');
-	if (allowMultiple) props['data-allow-multiple'] = '';
+	if (allowMultiple) props['data-allow-multiple'] = 'true';
 	return props;
 }
 
@@ -28,6 +28,32 @@ export function getHeadingProps(props) {
 	return returnProps;
 }
 
+// id: Context から取得できればそれを優先（React用）
+export function getPanelProps({ lismClass, _contextID, accID = '__LISM_ACC_ID__', isOpen = false, ...props }) {
+	// panel側は必要最低限のプロパティのみ。
+	const panelProps = {
+		lismClass: atts(lismClass, 'c--accordion_panel'),
+		id: _contextID || accID,
+		hidden: isOpen ? undefined : 'until-found',
+		pos: 'relative',
+		ov: 'hidden',
+	};
+
+	// 余白などその他の指定は全て _content 側へ渡す
+	const contentProps = { lismClass: 'c--accordion_content', layout: 'flow', ...props };
+
+	// content側へ移すprops
+	// ['p', 'px', 'py', 'pl', 'pr', 'pt', 'pb', 'px-s', 'px-e', 'py-s', 'py-e'].forEach((prop) => {
+	// 	if (props[prop]) {
+	// 		contentProps[prop] = props[prop];
+	// 		delete props[prop];
+	// 	}
+	// });
+
+	// 2つ返す
+	return { panelProps, contentProps };
+}
+
 export const defaultProps = {
 	// header: { lismClass: 'c--accordion_header' },
 	button: {
@@ -36,17 +62,16 @@ export const defaultProps = {
 		layout: 'flex',
 		setPlain: 1,
 		g: '10',
+		w: '100%',
 		ai: 'center',
+		jc: 'between',
 	},
 	icon: {
 		lismClass: 'c--accordion_icon a--icon',
 		tag: 'span',
+		// d: 'grid',
+		pi: 'center',
+		fxsh: '0',
 		'aria-hidden': 'true',
-	},
-	panel: {
-		lismClass: 'c--accordion_panel',
-	},
-	content: {
-		lismClass: 'c--accordion_content',
 	},
 };
