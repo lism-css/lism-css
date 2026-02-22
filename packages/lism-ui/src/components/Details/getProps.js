407bdfa8695ff577fcd5803cf423e3c4068bc2d2
ddryo <dd.bocsfcryo@gmail.com>
2026-02-22 15:21:45 +0900

    Details に setPlain 使用

--- packages/lism-ui/src/components/Details/getProps.js
+++ packages/lism-ui/src/components/Details/getProps.js
@@ -16,7 +16,7 @@ export function getDetailsProps({ lismClass, ...props }) {
  */
 export const defaultProps = {
 	summary: { lismClass: 'c--details_summary', layout: 'flex', g: '10', ai: 'center' },
-	title: { lismClass: 'c--details_title', tag: 'span', fx: '1' },
+	title: { lismClass: 'c--details_title', tag: 'span', fx: '1', setPlain: 1 },
 	icon: { lismClass: 'c--details_icon a--icon', tag: 'span', 'aria-hidden': 'true' },
 	body: { lismClass: 'c--details_body' },
 	content: { lismClass: 'c--details_content', layout: 'flow', flow: 's' },
