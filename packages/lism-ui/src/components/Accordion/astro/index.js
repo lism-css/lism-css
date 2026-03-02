a75db879a0fdc82b9bbea6c063536ae8f8d7a92c
ddryo <31400297+ddryo@users.noreply.github.com>
2026-03-02 11:30:36 +0900

    accordion export 修正

--- packages/lism-ui/src/components/Accordion/astro/index.js
+++ packages/lism-ui/src/components/Accordion/astro/index.js
@@ -1,11 +1,11 @@
 import Root from './Root.astro';
 import Item from './Item.astro';
-import Header from './Header.astro';
 import Heading from './Heading.astro';
 import Button from './Button.astro';
 import Icon from './Icon.astro';
 import Panel from './Panel.astro';
 
 // Header: Heading, Button, Iconのセット
+// Body: Panel の別名（ドキュメント向け）
 
-export default { Root, Item, Header, Heading, Panel, Icon, Button };
+export default { Root, Item, Heading, Panel, Icon, Button };
