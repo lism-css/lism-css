0ec27eecaf451e03e8a3af1528ee56de5e1ab046
torounit <mail@torounit.com>
2026-03-20 09:24:23 +0900

    fix:StyleWithCustomProps での key を string に緩める

--- packages/lism-css/src/lib/types.ts
+++ packages/lism-css/src/lib/types.ts
@@ -1,4 +1,4 @@
 import { type CSSProperties } from 'react';
 
 // CSS Custom Properties を含むスタイル型
-export type StyleWithCustomProps = CSSProperties & Record<`--${string}`, string | number | undefined>;
+export type StyleWithCustomProps = CSSProperties & Record<string, string | number | undefined>;
