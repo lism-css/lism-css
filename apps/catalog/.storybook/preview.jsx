2c740fa5b618eb4d20e548584aa9754e6d1fc88c
torounit <mail@torounit.com>
2026-03-24 12:57:30 +0900

    fix: hasGutter -> setGutter

--- apps/catalog/.storybook/preview.jsx
+++ apps/catalog/.storybook/preview.jsx
@@ -147,7 +147,7 @@ const preview = {
 		isWrapper: { control: 'object', table: { category: 'State' } },
 		isLayer: { control: 'boolean', table: { category: 'State' } },
 		isLinkBox: { control: 'boolean', table: { category: 'State' } },
-		hasGutter: { control: 'boolean', table: { category: 'State' } },
+		setGutter: { control: 'boolean', table: { category: 'State' } },
 		isVertical: { control: 'boolean', table: { category: 'State' } },
 	},
 	parameters: {
