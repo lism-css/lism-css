ee5b7ce6733939797b52c754e93a888fb9f76b5d
torounit <mail@torounit.com>
2026-03-24 12:33:20 +0900

    feat: Icon の story に外部コンポーネントを追加

--- packages/lism-css/src/components/atomic/Icon/Icon.stories.tsx
+++ packages/lism-css/src/components/atomic/Icon/Icon.stories.tsx
@@ -1,4 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
+import { Loader2, Play } from 'lucide-react';
+
 import { Icon } from './index';
 import presets from './presets';
 import type { PresetIconName } from './getProps';
@@ -81,4 +83,21 @@ export const WithViewBox: Story = {
 			<path d='M247.15,212.42l-56-112a8,8,0,0,0-14.31,0l-21.71,43.43A88,88,0,0,1,108,126.93,103.65,103.65,0,0,0,135.69,64H160a8,8,0,0,0,0-16H104V32a8,8,0,0,0-16,0V48H32a8,8,0,0,0,0,16h87.63A87.76,87.76,0,0,1,96,116.35a87.74,87.74,0,0,1-19-31,8,8,0,1,0-15.08,5.34A103.63,103.63,0,0,0,84,127a87.55,87.55,0,0,1-52,17,8,8,0,0,0,0,16,103.46,103.46,0,0,0,64-22.08,104.18,104.18,0,0,0,51.44,21.31l-26.6,53.19a8,8,0,0,0,14.31,7.16L148.94,192h70.11l13.79,27.58A8,8,0,0,0,240,224a8,8,0,0,0,7.15-11.58ZM156.94,176,184,121.89,211.05,176Z' />
 		</Icon>
 	),
+};
+
+export const ExternalComponent: Story = {
+	name: '外部コンポーネント（lucide-react）',
+	args: {
+		as: Play,
+		fz: '2rem',
+	},
+};
+
+export const ExternalComponentSpinner: Story = {
+	name: '外部コンポーネント（Loader2）',
+	args: {
+		as: Loader2,
+		fz: '2rem',
+		c: 'blue',
+	},
 };
