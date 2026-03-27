462f2756d2b1a5d1b172f363dd1a241c1e116fa5
torounit <mail@torounit.com>
2026-03-27 18:15:04 +0900

    fix: *.astro への型定義と eslint でのエラー検出

--- packages/lism-css/packages/astro/env.d.ts
+++ packages/lism-css/packages/astro/env.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="astro/client" />
 
+declare module '*.astro' {
+	const component: (props: Record<string, unknown>) => unknown;
+	export default component;
+}
+
 declare module 'astro:assets' {
 	export const Image: typeof import('astro/components/Image.astro').default;
 }
