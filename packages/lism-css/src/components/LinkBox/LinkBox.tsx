8f6fc17d99de0191257042eb5f2207d3f8fdd675
torounit <mail@torounit.com>
2026-03-31 15:19:11 +0900

    fix: LinkBox の挙動を修整。as === a の時、href が無かったら置き換え。

--- packages/lism-css/src/components/LinkBox/LinkBox.tsx
+++ packages/lism-css/src/components/LinkBox/LinkBox.tsx
@@ -2,9 +2,12 @@ import { Lism, type LismComponentProps } from '../Lism';
 
 type LinkBoxProps = LismComponentProps<'a'>;
 
-export default function LinkBox({ children, ...props }: LinkBoxProps) {
+export default function LinkBox({ as = 'a', children, ...props }: LinkBoxProps) {
 	const hasHref = !!props.href;
-	const tag = hasHref ? 'a' : 'div';
+	// LinkBoxは基本的にリンク要素（aタグ）として機能するため、
+	// hrefがないかつ、aタグとしてレンダリングする場合はdivタグに置き換える
+	const tag = !hasHref && as === 'a' ? 'div' : as;
+
 	return (
 		<Lism isLinkBox as={tag} {...props}>
 			{children}
