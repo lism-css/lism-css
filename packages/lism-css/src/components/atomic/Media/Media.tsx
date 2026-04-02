086522d498c6c9313040e6550f22d6f6398645f9
ddryo <31400297+ddryo@users.noreply.github.com>
2026-04-02 16:09:34 +0900

    fix: Mediaドキュメントのa--media記述を削除し_media.scssを廃止

--- packages/lism-css/src/components/atomic/Media/Media.tsx
+++ packages/lism-css/src/components/atomic/Media/Media.tsx
@@ -4,6 +4,6 @@ import getProps, { type MediaOwnProps } from './getProps';
 
 type MediaComponentProps<T extends ElementType = 'img'> = LismComponentProps<T> & MediaOwnProps;
 
-export default function Media<T extends ElementType = 'img'>({ ...props }: MediaComponentProps<T>) {
+export default function Media<T extends ElementType = 'img'>(props: MediaComponentProps<T>) {
   return <Lism as="img" {...getProps(props)} />;
 }
