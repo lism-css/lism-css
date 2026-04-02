398836531dccefc390c8e74fdc5387de90e09b2b
ddryo <31400297+ddryo@users.noreply.github.com>
2026-04-03 02:24:21 +0900

    refactor: MediaコンポーネントのgetMediaPropsを削除しpropsを直接渡すように簡素化

--- packages/lism-css/src/components/atomic/Media/getProps.ts
+++
@@ -1,7 +0,0 @@
-import type { LismProps } from '../../../lib/getLismProps';
-
-export type MediaProps = LismProps;
-
-export default function getMediaProps({ lismClass, style = {}, ...rest }: MediaProps): LismProps {
-  return { ...rest, lismClass, style };
-}
